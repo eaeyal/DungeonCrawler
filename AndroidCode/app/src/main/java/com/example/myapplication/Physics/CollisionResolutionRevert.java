a8dac75659c422ff2866f093e11a2a0b08eece8b
sqz269 <yin2690855381@gmail.com>
2023-10-22 19:49:43 -0400

    Fixed check style error

--- AndroidCode/app/src/main/java/com/example/myapplication/Physics/CollisionResolutionRevert.java
+++ AndroidCode/app/src/main/java/com/example/myapplication/Physics/CollisionResolutionRevert.java
@@ -2,10 +2,9 @@ package com.example.myapplication.Physics;
 
 import android.util.Log;
 
-import com.example.myapplication.Model.Player;
-
 public class CollisionResolutionRevert implements CollisionResolutionStrategy {
     public void resolveCollision(CollisionInfo collisionInfo) {
-        Log.d("CollisionResolutionRevert", "Collision resolution revert. CollisionInfo: " + collisionInfo.toString());
+        Log.d("CollisionResolutionRevert",
+                "Collision resolution revert. CollisionInfo: " + collisionInfo.toString());
     }
 }
