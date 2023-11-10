0d13a95a5c407a702f90f9f7b036c860c04c068d
eaeyal <eeyal3@gatech.edu>
2023-11-10 12:42:24 -0500

    Made enemies appear in room

--- AndroidCode/app/src/main/java/com/example/myapplication/Model/Olaf.java
+++ AndroidCode/app/src/main/java/com/example/myapplication/Model/Olaf.java
@@ -6,6 +6,7 @@ public class Olaf implements Enemy {
     private int healthPoints;
     private int attackDamage;
     private int speed;
+    private int direction;
 
     public int getSpriteId() {
         return spriteId;
@@ -55,8 +56,8 @@ public class Olaf implements Enemy {
 
     @Override
     public void startPos() {
-        this.setX(100);
-        this.setY(100);
+        this.setX(600);
+        this.setY(950);
     }
 
     // Enemy's attack pattern
@@ -68,7 +69,7 @@ public class Olaf implements Enemy {
     @Override
     public void movement() {
         //300 should be replaced with the value that prevents it from going offscreen
-        if (this.getX() < 50 ) {
+        /* if (this.getX() < 50 ) {
             this.setX(this.getX() + 10);
             this.setY(this.getY() + 10);
             this.setSpeed(20);
@@ -76,6 +77,29 @@ public class Olaf implements Enemy {
             this.setX(this.getX() - 10);
             this.setY(this.getY() - 10);
             this.setSpeed(10);
+        } */
+        switch (direction) {
+            case 0:
+                // move up
+                this.setY(this.getY() - 21);
+                direction++;
+                break;
+            case 1:
+                // move down
+                this.setY(this.getY() + 21);
+                direction++;
+                break;
+            case 2:
+                // move left
+                this.setX(this.getX() - 21);
+                direction++;
+                break;
+            case 3:
+                this.setX(this.getX() + 21);
+                direction = 0;
+                break;
+            default:
+                break;
         }
 
     }
